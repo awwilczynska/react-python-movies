62523c92e0a2a366c44e8e165c629f5db7498236
Student <student@d17-327e-pc-10.ipa.ii.agh.edu.pl>
2026-01-18 11:11:45 +0100

    dodanie usuwania filmow

--- ui/src/App.js
+++ ui/src/App.js
@@ -32,13 +32,23 @@ function App() {
         }
     }
 
+    async function handleDeleteMovie(movie) {
+        const url = `/movies/${movie.id}`;
+        const response = await fetch(url, {
+            method: 'DELETE'
+        });
+        if (response.ok) {
+            setMovies(movies.filter(m => m !== movie));
+        }
+    }
+
     return (
         <div className="container">
             <h1>My favourite movies to watch</h1>
             {movies.length === 0
                 ? <p>No movies yet. Maybe add something?</p>
                 : <MoviesList movies={movies}
-                              onDeleteMovie={(movie) => setMovies(movies.filter(m => m !== movie))}
+                              onDeleteMovie={handleDeleteMovie}
                 />}
             {addingMovie
                 ? <MovieForm onMovieSubmit={handleAddMovie}
